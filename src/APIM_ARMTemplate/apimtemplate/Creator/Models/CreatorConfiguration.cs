e2e15506259780fbfa5cb42608e984314923a361
Maxime LABELLE <maxime.labelle@chanel.com>
2020-11-20 16:25:58 +0100

    Supports product subscriptions

--- src/APIM_ARMTemplate/apimtemplate/Creator/Models/CreatorConfiguration.cs
+++ src/APIM_ARMTemplate/apimtemplate/Creator/Models/CreatorConfiguration.cs
@@ -94,6 +94,7 @@ namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Create
         public string policy { get; set; }
         // coma separated names
         public string groups { get; set; }
+        public List<SubscriptionConfig> subscriptions { get; set; }
     }
 
     public class PropertyConfig : PropertyResourceProperties
