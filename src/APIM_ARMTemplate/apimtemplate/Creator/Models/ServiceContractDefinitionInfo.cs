b784c8d597900580592069091751c15c5d5cd882
Maxime LABELLE <maxime.labelle@chanel.com>
2021-10-21 19:22:07 +0200

    WIP WIP

---
+++ src/APIM_ARMTemplate/apimtemplate/Creator/Models/ServiceContractDefinitionInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Azure.Management.ApiManagement.ArmTemplates.Create
+{
+    public sealed class ServiceContractDefinitionInfo
+    {
+        // one of swagger[-link]-json or openapi[-link|+json]
+        public string Format { get; set; }
+        // either an uri or a path to a file
+        public string Value { get; set; }
+    }
+}
